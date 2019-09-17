d73605fc6fcae65b84a8e84580debc5b3b8a747d
hurman_kyo <hurman_kyo@outlook.com>
2019-09-17 17:48:32 +0800

    aa

---
+++ src/main/java/com/example/demoXu/DemoXuApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoXu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoXuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoXuApplication.class, args);
+	}
+
+}
